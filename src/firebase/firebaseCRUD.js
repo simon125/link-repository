1f7b1c017e9a000ad54d971324385b003e509b68
Szymon <szymqwerty@gmail.com>
2020-07-12 14:39:35 +0200

    New endpoints for CRUD links and groups

--- src/firebase/firebaseCRUD.js
+++ src/firebase/firebaseCRUD.js
@@ -13,9 +13,25 @@ export const addLink = (link, user) => {
   });
 };
 
+export const removeLink = (id) => {
+  return db.collection('links').doc(id).delete();
+};
+
+export const updateLink = (id, link) => {
+  return db.collection('links').doc(id).update(link);
+};
+
 export const addGroup = (group, user) => {
   return db.collection('groups').add({
     userUid: user.uid,
-    group,
+    value: group,
   });
+};
+
+export const removeGroup = (id) => {
+  return db.collection('groups').doc(id).delete();
+};
+
+export const updateGroup = (id, optionName) => {
+  return db.collection('groups').doc(id).update({ value: optionName });
 };
